7b03efca9c3bbe82ce9f7fd67d4e95a45e0b1796
Chuck Lipari <chuck.lipari@microchip.com>
2023-09-15 16:14:39 -0700

    MH3-85719  crypto_apps_pic32cz_ca90:  Regenerated. Cleanup

--- apps/hsm_host_sb_pic32cz_ca90_m7/firmware/src/config/default/definitions.h
+++ apps/hsm_host_sb_pic32cz_ca90_m7/firmware/src/config/default/definitions.h
@@ -56,11 +56,11 @@
 #include "peripheral/nvic/plib_nvic.h"
 #include "peripheral/systick/plib_systick.h"
 #include "bsp/bsp.h"
+#include "system/time/sys_time.h"
 #include "system/int/sys_int.h"
 #include "system/cache/sys_cache.h"
 #include "osal/osal.h"
 #include "system/debug/sys_debug.h"
-#include "system/time/sys_time.h"
 #include "app.h"
 
 
